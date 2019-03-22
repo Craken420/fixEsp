2f5bd60fda4abd971d9152257b5c713bcc7cd0fb
Luis Angel <luis.pena.15s@utzmg.edu.mx>
2019-03-22 17:46:56 -0600

    Se agrego las funcionalidades en caso de que exista el archivo, el componente o el campo

--- fixEsp.js
+++ fixEsp.js
@@ -1,96 +1,227 @@
 const fs = require('fs')
 
-const add = require('./Utilities/RegExp/add')
+const { adapt } = require('./Utilities/RegExp/adapt')
 const cls = require('./Utilities/RegExp/cls')
 const take = require('./Utilities/RegExp/take')
 
 const code = require('./Utilities/Mix/Coding/codingFile')
-const duplex = require('./Utilities/Mix/Array/duplicate')
-const editFile = require('edit-file')
+const { fileExists } = require('./Utilities/Mix/Verify/fileExist')
+const { getFiles } = require('./Utilities/Mix/PathWay/getFiles')
+const { nameFile } = require('./Utilities/Intls/Components/nameFileInCmp')
 const newPath = require('./Utilities/Intls/PathWay/newPath')
 const supr = require('./Utilities/Mix/PathWay/deleteFile')
 
-const { fileExists } = require('./Utilities/Mix/Verify/fileExist')
-const { getFiles } = require('./Utilities/Mix/PathWay/getFiles')
-const { isNewOrExist } = require('./Utilities/Intls/Fields/isNewOrExist')
-const { nameFileInComp } = require('./Utilities/Intls/Components/nameFileInCmp')
+const { asComp } = require('./Utilities/Intls/Components/asComp')
+const duplex = require('./Utilities/Mix/Array/duplicate')
+const { isNewOrExistPro } = require('./Utilities/Intls/Fields/isNewOrExist')
 
 const dir = 'Testing\\'
+// const dir = 'C:\\Users\\lapena\\Documents\\Luis Angel\\Sección Mavi\\Intelisis\\Intelisis5000\\Reportes MAVI\\'
+
+function proccesComponentExist(comp, pathFile) {
+
+    let headNewComp = take.intls.comp.head(comp)
+
+    if(headNewComp) {
+
+        let newCompFields = take.intls.field.full(comp)
+
+        if (newCompFields) {
+            let crashFields = []
+            newCompFields.forEach(fieldSend => {
+
+                let olderComp = take.intls.comp.byName(
+                    headNewComp.join(''),
+                    code.getTxtInOriginCoding(pathFile).replace(/^\n+/gm, '\n')
+                )
+
+                if (olderComp) {
+
+                    olderComp =  duplex.del(olderComp)
+
+                    if (olderComp.length != 0) {
+                        if (!isNewOrExistPro(fieldSend, olderComp.join('\n'))) {
+                           
+                            let rgxAddField = new RegExp(`(?<=\\[\\b${adapt.toRegExp(headNewComp.join(''))}\\b\\])`,`g`)
+
+                            fs.appendFileSync('Reporte.txt','\n\nExist Comp: [' + headNewComp.join('') + ']')
+                            fs.appendFileSync('Reporte.txt','\nNew Field In Exist Comps: \n' + fieldSend)
+                           
+                            fs.writeFileSync(pathFile, code.getTxtInOriginCoding(pathFile).replace(rgxAddField, '\n' + fieldSend), 'latin1')
+                
+                        } else {
+                            crashFields.push(fieldSend)
+                        }
+                    }
+                }
+            })
+            if (crashFields) {
+                if (crashFields.length != 0) {
+
+                    fs.appendFileSync('Reporte.txt','\nColition fields: \n' + crashFields)
+                
+                    let colitiontext = `\n;Campos Colisionados\n;[${headNewComp.join('')}]\n${crashFields.map(x => x.replace(/^/, ';')).join('\n') + '\n'}`
+                    
+                    fs.appendFileSync(pathFile, colitiontext)
+                    
+                }
+            }
+        }
+    }
+}
+
+function proccesComponentNotExist(comp, headNewComp, pathFile){
+
+    fs.appendFileSync(pathFile, '\n[' + headNewComp + ']\n')
+
+    let newCompFields = take.intls.field.full(comp)
+
+    fs.appendFileSync('Reporte.txt','\n\nAdd New Comp: \n[' + headNewComp + ']')
+
+    if (newCompFields) {
+
+        let colition = []
+        newCompFields.forEach(field => {
+            if (new RegExp (`^${take.intls.field.name(field)}=`, `gm`).test(
+                    take.intls.comp.byName(
+                        headNewComp,
+                        code.getTxtInOriginCoding(pathFile).replace(/^\n+/gm, '\n')
+                    ).join(''))
+                ) {
+
+                colition.push(field)
+            } else {
+                fs.appendFileSync('Reporte.txt', '\n' + field)
+                fs.appendFileSync(pathFile, field + '\n')
+            }
+        })
+
+        if (colition) {
+            if (colition.length != 0) {
+                fs.appendFileSync('Reporte.txt','Colition fields: \n' + colition)
+                let textToSend =  `\n;Campos Colisionados\n;[${headNewComp}]\n${colition.map(x => x.replace(/^/, ';')).join('\n') + '\n'}`
+                fs.appendFileSync(pathFile,textToSend)
+            }
+        }
+    }
+    // console.log('\nFinish Content: \n',code.getTxtInOriginCoding(pathFile), '\n-------------------')
+}
+
+function addComponent(compsOutSide, nameFile, pathFile) {
+
+    let newComps = take.intls.comp.byNameFile(nameFile, compsOutSide.join('\n'))
+
+    newComps.forEach(comp => {
+
+        let headNewComp = take.intls.comp.head(comp)
+
+        if(headNewComp) {
+
+            let rgxHead = new RegExp(`^\\[\\b${adapt.toRegExp(headNewComp.join(''))}\\b\\]`, `gm`)
+
+            if (!rgxHead.test(code.getTxtInOriginCoding(pathFile))) {
+                proccesComponentNotExist(comp, headNewComp.join(''), pathFile)
+            } else {
+                proccesComponentExist(comp, pathFile)
+            }
+        }
+    })
+}
+
+function oddComponent(comp, pathFile) {
+
+    let headNewComp = take.intls.comp.head(comp)
+    // console.log(headNewComp)
+    if(headNewComp) {
+
+        let rgxHead = new RegExp(`^\\[\\b${adapt.toRegExp(headNewComp.join(''))}\\b\\]`, `gm`)
+
+        if (!rgxHead.test(code.getTxtInOriginCoding(pathFile))) {
+            proccesComponentNotExist(comp, headNewComp.join(''), pathFile)
+        } else {
+            proccesComponentExist(comp, pathFile)
+        }
+    }
+
+}
 
 getFiles(dir, ['.esp'])
     .then(files => {
 
         files.forEach(file => {
 
-            supr.deleteEmptyFile(file)
+            if (file != dir + 'Personal_TBL_MAVI.esp') {
+                // if (file == dir + 'ActivarDesafectar.esp' || file == dir + 'MonederoElectronico.esp'|| file == dir + 'UEN.esp'|| file == dir + 'ArtConDisponible_ANEXO_MAVI.esp'||file == dir + 'VentaT_Mayor12Meses.esp'||file == dir + 'EliminarConta.esp') {
+                if (supr.deleteEmptyFile(file)) {
+                    return
+                } else {
+                    let pathEsp = newPath.maviToEsp(file)
 
-            let pathEsp = newPath.maviToEsp(file)
+                    let txt = code.getTxtInOriginCoding(file)
 
-            let txt = code.getTxtInOriginCoding(file)
+                    if (pathEsp){
+                        fs.appendFileSync('Reporte.txt',
+                                '\n************************************\n' 
+                                + pathEsp
+                                + '\n************************************'
+                            )
+                        let compsOutSide = take.intls.comp.outSide(pathEsp, cls.intls.comments(txt))
 
-            let compsOutSide = take.intls.comp.outSide(pathEsp, cls.intls.comments(txt))
+                        if (compsOutSide) {
+                            
+                            compsOutSide.forEach(outComp => {
+                                // console.log(outComp)
+                                fs.appendFileSync('Reporte.txt',
+                                    '\n\n-------------------------'
+                                    + 'Comp OutSide:\n'
+                                    + outComp
+                                    +'\n-------------------'
+                                )
+                                let nameFileInCmp = nameFile(outComp)
+                                console.log('nameFileInCmp: ',nameFileInCmp)
+                                // let namesFiles = duplex.del(nameFileInComp(compsOutSide))
+                                
+                                // if (namesFiles) {
 
-            if (compsOutSide) {
+                                //     namesFiles.forEach(nameFile => {
 
-                let namesFiles = duplex.del(nameFileInComp(compsOutSide))
+                                //         if (nameFile) {
+    
+                                let newPathComp = newPath.espToMavi(nameFileInCmp)
+                                // console.log('newPathComp: ',newPathComp)
+                                if (!fileExists(dir + newPathComp)) {
+                                    fs.appendFileSync('Reporte.txt','\nCreate file: ' + newPathComp)
+                                    fs.writeFileSync(dir + newPathComp, ';*** 22-03-19 Reacomodo de los archivos especiales\n', 'latin1')
+                                //     addComponent(compsOutSide, nameFile, dir + newPathComp)
+                                } else {
+                                    fs.appendFileSync('Reporte.txt','\nExist File: ' + newPathComp)
+                                }
+                                
+                                oddComponent(outComp, dir + newPathComp)
+                                // addComponent(compsOutSide, nameFile, dir + newPathComp)
 
-                if (namesFiles) {
-                    namesFiles.forEach(nameFile => {
 
-                        let newPathComp = newPath.espToMavi(nameFile)
-                        let newComps = take.intls.comp.byNameFile(nameFile,compsOutSide.join('\n'))
+                                            
 
-                        if (fileExists(dir + newPathComp)) {
+                                //         }
+                                //     })
+                                // }
+                                fs.writeFileSync(file, cls.intls.comp.outSide(pathEsp, txt), 'latin1')
 
-                            editFile(dir + newPathComp, text => {
-
-                                newComps.forEach(newComp => {
-
-                                    let headNewComp = take.intls.comp.head(newComp)
-
-                                    if(headNewComp) {
-
-                                        let newCompFields = take.intls.field.full(newComp)
-
-                                        if (newCompFields) {
-
-                                            let olderComp = take.intls.comp.byName(
-                                                headNewComp.join(''),
-                                                code.getTxtInOriginCoding(dir + newPathComp).replace(/^\n+/gm, '\n')
-                                            )
-
-                                            if (olderComp) {
-
-                                                olderComp =  duplex.del(olderComp)
-
-                                                if (olderComp.length != 0) {
-                                                    //que solo devuelta true si existe y false si no
-                                                    let lol = isNewOrExist(newCompFields, olderComp[0])
-
-                                                    if (lol['newField']) {
-                                                        if (lol['newField']!= 0) {
-                                                            text = add.intls.comp.addInTheEnd(headNewComp, '\n' + lol['newField'].join('\n'), text)
-                                                        }
-                                                    }
-
-                                                    // if (lol['crashField']) {
-                                                    //     if (lol['crashField']!= 0) {
-                                                    //         text = text.replace(/$/, `\n;Campos Colisionados\n;[${headNewComp}]\n${lol['crashField'].join('\n').replace(/^/g, ';')}`)
-                                                    //     }
-                                                    // }
-                                                }
-                                            }
-                                        }
-                                    }
-                                })
-                                return text
                             })
-                        } else {
-                            // console.log('Creado: ',dir + key)
-                            // fs.writeFileSync(dir + newPathComp, newComps.join(\n))
+                            if (!asComp(file)) {
+                                fs.appendFileSync('Reporte.txt',
+                                    '\n_______________'
+                                    + '\nNo Comp: Delete: ' 
+                                    + file.replace(/.*\/|.*\\/, '')
+                                    +'\n_______________'
+                                )
+                                fs.unlinkSync(file)
+                            }
                         }
-                    })
+                    }
                 }
+                //}
             }
         })
     })
