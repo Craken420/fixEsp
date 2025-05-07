a2b1feabf3c70061646c044f36802a3dd97cb557
Luis Angel Peña <la.pena@mavi.mx>
2025-05-06 18:44:52 -0600

    Completar código

--- fixEsp.js
+++ fixEsp.js
@@ -2,18 +2,71 @@ const fs = require('fs')
 const path = require('path')
 const R = require('ramda')
 
-const { DrkBx } = require('./DarkBox/index')
+const patt = {
+    //=> _FRM_
+    abbrtObjBtwnLowScripts: /(?<=_)(dlg|frm|rep|tbl|vis)(?=_)/i,
+    lastPointToLowScript: txt => txt.replace( /\.(?!.*?\.)/, '_'),
+    ext: /\.\w{3}$/,
+    untilExt: /.*?(?=\.\w+$)/,
+    cmpNameFile: /(?<=^\[).*?(?=\/.*?\])/g,
+}
+
+const methods = {
+    pthRoot: R.pipe(
+        R.split(/[\\/]/),      // Divide por / o \ (para Windows y Linux)
+        R.last                 // Toma el último segmento (el nombre del archivo)
+    ),
+    fldsToObj: R.pipe(
+        take.fldFull,
+        R.map(R.split(/=/))
+    ),
+    espToMavi: pathFile => {
+            return patt.lastPointToLowScript(
+                pathFile.replace(patt.ext, patt.allUntilExt(pathFile).toUpperCase())
+            ) + '_MAVI.esp'
+    },
+    cmpsToTxt: R.pipe(
+        R.map(R.toPairs),
+        R.map(fldsToTxt),
+        R.toPairs,
+        R.map(R.join(']\n')),
+        R.map(R.replace(/^/, '['))
+    ),
+    addCmpExst: R.curry( (exstCmp, txt) => {
+        // console.log(mergCmpWithCmpInTxt(exstCmp, txt).join(''))
+        return R.replace(
+            cmpByNameNoAdapt(cmpHead(exstCmp)),
+            mergCmpWithCmpInTxt(exstCmp, txt).join('') + '\n',
+            txt
+        )
+    }),
+    outSide: (nameFileInHead, txt) => {
+        if (make.outSide(nameFileInHead).test(txt)) {
+            return txt.replace(make.outSide(nameFileInHead), '')
+        } else {
+            return txt
+        }
+    },
+    toRgxHeadComp: nameComp => new RegExp(`^\\[\\b${nameComp}\\b\\]`, `gm`),
+    toRgxHeadCmp: R.pipe(this.cmpHead, this.toRgxHeadComp),
+    checkExstHeadCmpInTxt: R.curry( (comp, txt) => R.test( toRgxHeadCmp(comp), txt )),
+    existAndIsFile: R.both(fs.existsSync, isFl),
+    allUntilExt: txt => txt.replace(patt.untilExt, ''),
+    getLatinTxt: file => fs.readFileSync(file, 'Latin1'),
+    getFiltFls: R.curry( (ext, files) => filterFiles( ext, getPathsFiles(files) ) ),
+    cmpHead: txt => (patt.cmpHead.test(txt.replace(/^/, '\n'))) ? txt.replace(/^/, '\n').match(patt.cmpHead) : false,
+    cmpNameFile: txt => patt.cmpNameFile.test(txt) && txt.match(patt.cmpNameFile),
+    addCmpInexstInTxt: R.curry( (InxstCmp, txt) => {
+        txt = txt + '\n' + InxstCmp + '\n'
+        return txt
+    })
+}
 
 const rootData = 'Testing\\'
 
 const cnctRootEsp = R.map(file => rootData + file)
 
 const omitFls = R.without( cnctRootEsp( ['Personal_TBL_MAVI.esp'] ) )
-
-const print = value => {
-    console.log(value)
-    return value
-}
 
 const fileIsEmpty = R.pipe(
     fs.readFileSync,
@@ -24,12 +77,12 @@ const toEsp = R.pipe(
     path.parse,
     R.prop('name'),
     R.replace(/_MAVI/gi, ''),
-    DrkBx.mix.change.lastLowScriptToPoint,
-    R.replace(DrkBx.mix.patt.pthExt, R.toLower)
+    lastLowScriptToPoint,
+    R.replace(patt.ext, R.toLower)
 )
 
-const verifyAndToEsp = paths => (DrkBx.intls.patt.abbrtObjBtwnLowScripts)
-    ? toEsp(paths) : DrkBx.mix.cls.pthRoot(paths).replace(/(\_|\.).*/g, '')
+const verifyAndToEsp = paths => (patt.abbrtObjBtwnLowScripts)
+    ? toEsp(paths) : methods.pthRoot(paths).replace(/(\_|\.).*/g, '')
 
 const delEmpty = path => {
     if (fileIsEmpty(path) == '') {
@@ -42,14 +95,14 @@ const delEmpty = path => {
 }
 
 const espFiltFls = R.pipe(
-    DrkBx.mix.fls.getFiltFls,
+    methods.getFiltFls,
     omitFls,
     R.map(delEmpty),
     R.filter(Boolean)
 )
 
-const cmpToObj = cmp => R.objOf( DrkBx.intls.take.cmpHead(cmp), DrkBx.intls.fnObj.fldsToObj(cmp) )
-const cmpToObjWithBrakets = cmp => R.objOf('[' + DrkBx.intls.take.cmpHead(cmp) + ']', DrkBx.intls.fnObj.fldsToObj(cmp) )
+const cmpToObj = cmp => R.objOf( methods.cmpHead(cmp), methods.fldsToObj(cmp) )
+const cmpToObjWithBrakets = cmp => R.objOf('[' + methods.cmpHead(cmp) + ']', methods.fldsToObj(cmp) )
 
 const uniqObjs = array => {
     const newObj = {}
@@ -58,27 +111,24 @@ const uniqObjs = array => {
 }
 
 const gtUniqCmpsOutSide = R.pipe(
-    DrkBx.intls.take.cmpOutSide,
+    methods.cmpOutSide,
     R.filter(Boolean),
     R.map(cmpToObj),
     uniqObjs,
-    DrkBx.intls.fnObj.cmpsToTxt,
+    methods.cmpsToTxt,
 )
 
-const rootEsp = '../../../Intelisis/Intelisis5000/Reportes MAVI\\'
-const rootOrig = '../../../Intelisis/Intelisis5000/Codigo Original\\'
-
 const proccessFile = pathFile => {
-    return gtUniqCmpsOutSide( verifyAndToEsp(pathFile), DrkBx.mix.fls.gtLtnTxt(pathFile) )
+    return gtUniqCmpsOutSide( verifyAndToEsp(pathFile), methods.getLatinTxt(pathFile) )
 }
 
-const nameFile = cmp => DrkBx.intls.newPath.espToMavi(DrkBx.intls.take.cmpNameFile(cmp).join(''))
+const nameFile = cmp => methods.espToMavi(methods.cmpNameFile(cmp).join(''))
 
 const addIndex = cmp => R.objOf(nameFile(cmp), cmpToObj(cmp))
 
 const isntExstCreate = (comps, key) => {
     if (comps.length != 0) {
-        if (!DrkBx.mix.fls.exist('Testing\\' + key)) {
+        if (!methods.existAndIsFile('Testing\\' + key)) {
             fs.writeFileSync('Testing\\' + key, 'Reacomodo:\n\n')
         }
     }
@@ -86,17 +136,17 @@ const isntExstCreate = (comps, key) => {
 }
 
 const addCmp = R.curry( (comp, text) => {
-    if (DrkBx.intls.fnCmp.checkExstHeadCmpInTxt(comp,text)) {
-        text = DrkBx.intls.fnCmp.addCmpExst(comp, text)
+    if (methods.checkExstHeadCmpInTxt(comp,text)) {
+        text = methods.addCmpExst(comp, text)
     }
     else {
-        text = DrkBx.intls.fnCmp.addCmpInexst(comp,text)
+        text = methods.addCmpInexst(comp,text)
     }
     return text
 })
 
 const addCmps = R.curry( (comps, nameFile) => {
-    let text = DrkBx.mix.fls.gtLtnTxt('Testing\\' + nameFile)
+    let text = methods.getLatinTxt('Testing\\' + nameFile)
     comps.forEach(comp => {
         text = addCmp(comp, text)
     })
@@ -105,8 +155,8 @@ const addCmps = R.curry( (comps, nameFile) => {
 
 
 const clean = path => {
-    if (DrkBx.intls.make.outSide(DrkBx.intls.newPath.maviToEsp(path)).test(DrkBx.mix.fls.gtLtnTxt(path))) {
-        fs.writeFileSync(path, DrkBx.intls.cls.outSide(DrkBx.intls.newPath.maviToEsp(path), DrkBx.mix.fls.gtLtnTxt(path)), 'Latin1')
+    if (methods.outSide(methods.maviToEsp(path)).test(methods.getLatinTxt(path))) {
+        fs.writeFileSync(path, methods.outSide(methods.maviToEsp(path), methods.getLatinTxt(path)), 'Latin1')
         return path
     } else {
         return path
@@ -114,7 +164,7 @@ const clean = path => {
 }
 
 const delIfHasntCmp = path => {
-    if (!/^\[.*?\]((\n|\r)(?!(\n|)^\[.+?\]).*?$)+/gm.test(DrkBx.mix.fls.gtLtnTxt(path))) {
+    if (!/^\[.*?\]((\n|\r)(?!(\n|)^\[.+?\]).*?$)+/gm.test(methods.getLatinTxt(path))) {
         console.log('delete: ', path)
         fs.unlinkSync(path)
         return { file: path, Status: 'Delete'}
@@ -132,7 +182,7 @@ const lols = R.pipe(
     proccessFile,
     R.map(addIndex),
     uniqObjs,
-    R.map(DrkBx.intls.fnObj.cmpsToTxt),
+    R.map(methods.cmpsToTxt),
     R.forEachObjIndexed(isntExstCreate),
     R.mapObjIndexed(addCmps),
     R.forEachObjIndexed( (text, path) => fs.writeFileSync('Testing\\' +path, text, 'Latin1')),
